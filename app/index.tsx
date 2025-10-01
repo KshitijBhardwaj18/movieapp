d4b20ebda41292adab8c985336e4a1259ced095c
Kshitij Bhardwaj <tothemoon202154@outlook.com>
2025-10-01 14:18:25 +0530

    setup nativewind and basic structure

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import "./global.css";
+
+export default function Index() {
+  return (
+    <View className="flex items-center justify-center flex-1">
+      <Text className="text-5xl text-primary">helllloo whatsupp</Text>
+    </View>
+  );
+}
