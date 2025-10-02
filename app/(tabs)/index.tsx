7cae09f3e66fb09efe11ccb3c3563fcc6105e3f9
Kshitij Bhardwaj <tothemoon202154@outlook.com>
2025-10-02 18:56:03 +0530

    add: home screen setup

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,15 @@
-import { Text, View } from "react-native";
-
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
+import { Image, ScrollView, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex items-center justify-center flex-1">
-      <Text className="text-5xl text-primary ">Welcome!</Text>
+    <View className="flex-1 bg-primary ">
+      <Image source={images.bg} className="w-full absolute z-0" />
+
+      <ScrollView className="flex-1 px-5" showsVerticalScrollIndicator={false} contentContainerStyle={{minHeight:'100%',paddingBottom:10}}>
+        <Image className="w-12 h-10  mt-20 mb-5  mx-auto" source={icons.logo} />
+      </ScrollView>
     </View>
   );
 }
